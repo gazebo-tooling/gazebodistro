cad3e683b18ad88a5b99069e5b187b5dc3142f0d
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-05-14 21:40:24 +0200

    Improve script findings

--- tools/gazebodistro_to_system_pkgs.bash
+++ tools/gazebodistro_to_system_pkgs.bash
@@ -5,20 +5,52 @@ fi
 
 GAZEBO_DISTRO=${1}
 
+remove_string_dups()
+{
+    local string=${1}
+
+    sed 's/ /\n/g' <<< $string | sort | uniq | tr '\n' ' '
+}
+
 echo
 echo "This tool reads a gazebodistro file and look in the system for"
 echo "all available pkgs corresponding to that distro file"
 echo
 
 BRANCH_NAMES=$(grep version: ${GAZEBO_DISTRO} | awk '{ print $2 }')
+# Get packages from gazebodistro file. Some assumptions and ros- filter
 total_pkgs=
 for branch in ${BRANCH_NAMES}; do
     pkg_name=${branch/ign-/ignition-}
     pkg_name=${pkg_name/sdf/sdformat}
-    echo " Searching: ${pkg_name}"
-    pkgs=$(apt-cache search ${pkg_name} | awk '{ print $1 }')
+    echo " 1. Getting packages using: ${pkg_name}"
+    # assume here than when foo1 fails search for foo
+    pkgs=$(apt-cache search ${pkg_name} | awk '{ print $1 }' | grep -v '^ros-') || pkgs=$(apt-cache search ${pkg_name/1/} | awk '{ print $1 }' | grep -v '^ros-')
     total_pkgs="${total_pkgs} ${pkgs}"
 done
 
-echo 
-echo ${total_pkgs}
+echo
+
+# Get all packages listed as dependencies
+all_deps_pkgs=
+for pkg in $(echo ${total_pkgs} | sort | uniq); do
+    echo " 2. Get dependencies of: ${pkg}"
+    new_pkgs=$(apt-rdepends ${pkg} 2>/dev/null | grep Depends: | awk '{ print $2 }' | sort | uniq)
+    all_deps_pkgs="${all_deps_pkgs} ${new_pkgs}"
+done
+
+all_deps_pkgs=$(remove_string_dups "${all_deps_pkgs}")
+echo
+
+for new_pkg in ${all_deps_pkgs}; do
+    if [[ -n $(apt-cache madison ${new_pkg} | grep 'packages.osrfoundation.org') ]]; then
+	echo " 3. Found packages.osrfoundation.org package ${new_pkg}"
+	osrf_pkgs="${osrf_pkgs} ${new_pkg}"
+    fi
+done
+
+osrf_pkgs=$(remove_string_dups "${osrf_pkgs}")
+echo
+
+echo "LIST OF PACKAGES:"
+echo "${osrf_pkgs}"
